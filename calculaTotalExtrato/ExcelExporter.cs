8eee234279ce50b97793270b6585edddd021253f
guiFerranti <guilhermecousseau@icloud.com>
2024-06-19 12:07:42 -0300

    refactor: transformando numeros para valor numero no excel e ajustando tamanho das colunas

--- calculaTotalExtrato/ExcelExporter.cs
+++ calculaTotalExtrato/ExcelExporter.cs
@@ -1,5 +1,6 @@
 ﻿using OfficeOpenXml;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 
 public class ExcelExporter
@@ -12,20 +13,41 @@ public class ExcelExporter
         {
             ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Extrato");
 
-            // Adicionar cabeçalhos
             worksheet.Cells[1, 1].Value = "Data";
             worksheet.Cells[1, 2].Value = "Lançamentos";
             worksheet.Cells[1, 3].Value = "Valor (R$)";
             worksheet.Cells[1, 4].Value = "Saldo (R$)";
 
-            // Adicionar dados
             for (int i = 0; i < tableData.Count; i++)
             {
                 worksheet.Cells[i + 2, 1].Value = tableData[i][0];
                 worksheet.Cells[i + 2, 2].Value = tableData[i][1];
-                worksheet.Cells[i + 2, 3].Value = tableData[i][2];
-                worksheet.Cells[i + 2, 4].Value = tableData[i][3];
+
+                if (decimal.TryParse(tableData[i][2], NumberStyles.Number, new CultureInfo("pt-BR"), out decimal valor))
+                {
+                    worksheet.Cells[i + 2, 3].Value = valor;
+                    worksheet.Cells[i + 2, 3].Style.Numberformat.Format = "#,##0.00";
+                }
+                else
+                {
+                    worksheet.Cells[i + 2, 3].Value = tableData[i][2];
+                }
+
+                if (decimal.TryParse(tableData[i][3], NumberStyles.Number, new CultureInfo("pt-BR"), out decimal saldo))
+                {
+                    worksheet.Cells[i + 2, 4].Value = saldo;
+                    worksheet.Cells[i + 2, 4].Style.Numberformat.Format = "#,##0.00";
+                }
+                else
+                {
+                    worksheet.Cells[i + 2, 4].Value = tableData[i][3];
+                }
             }
+
+            worksheet.Column(1).AutoFit();
+            worksheet.Column(2).AutoFit();
+            worksheet.Column(3).AutoFit();
+            worksheet.Column(4).AutoFit();
 
             FileInfo fileInfo = new FileInfo(filePath);
             excelPackage.SaveAs(fileInfo);
